be77a3104f7e6acf472ca626ec9bdf4b9d973643
Adriano Jr <adrianojbarros5@gmail.com>
2024-07-25 20:53:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# criptografador_de_mensagens
---
+++ app.js
@@ -0,0 +1,10 @@
+function criptografarTexto(){
+    let mensagem = document.getElementById('saida_texto_mensagem');
+    let frase = document.getElementById('saida_texto_frase');
+    mensagem.innerHTML = 'teste';
+    frase.innerHTML = null;
+}
+
+function descriptografarTexto(){
+    alert("Botão para descriptografar texto");
+}
