17d2b2eefd0bf28231504593b4074d7891967539
Adriano Jr <adrianojbarros5@gmail.com>
2024-07-26 12:37:21 -0300

    Add JS

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+let campoTexto = document.getElementById('entradaTexto').value;
+
 function criptografarTexto(){
-    let mensagem = document.getElementById('saida_texto_mensagem');
-    let frase = document.getElementById('saida_texto_frase');
-    mensagem.innerHTML = 'teste';
-    frase.innerHTML = null;
+        document.getElementById('saida_texto_imagem').style.display = 'none';
+        document.getElementById('saida_texto_frase').style.display = 'none';
+        document.getElementById('saida_texto_mensagem-aviso').style.display = 'none';
+        document.getElementById('saida_texto_mensagem').style.display = 'inline';
+        document.getElementById('copiarCriptografia').style.display = 'inline';
+        let textoCriptografar = document.getElementById('entradaTexto').value; // Captura o texto para cript
+        console.log(textoCriptografar);
+        document.getElementById('saida_texto_mensagem').style.color = '#495057';
+        let texto = document.getElementById('saida_texto_mensagem');
+        texto.innerHTML = textoCriptografar;
 }
 
 function descriptografarTexto(){
     alert("Botão para descriptografar texto");
+}    
+
+function recarregarPagina(){
+    location.reload();
 }
+
+/*
+Acrescentar criptografia da mensagem, criar a função do botão copiar, e acrescentar a estilização do textarea do campo de criptografia
+*/
