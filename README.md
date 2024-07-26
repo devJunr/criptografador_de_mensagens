e2bf8a5d58f27ddde6b3a8b69e25834b77d528d9
Adriano Jr <adrianojbarros5@gmail.com>
2024-07-26 20:46:49 -0300

    Add criptografia e funções

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
-# criptografador_de_mensagens
+![](https://kinsta.com/wp-content/uploads/2023/07/what-is-encryption.jpg)
+
+# Criptografador de Mensagens
+
+Este projeto foi desenvolvido com o objetivo de aprimorar o aprendizado em desenvolvimento web. Consiste em uma página web que permite a criptografia e descriptografia de mensagens usando uma tabela de códigos personalizada.
+
+## Funcionalidades
+
+- **Criptografia**: Converta suas mensagens em códigos secretos.
+- **Descriptografia**: Reverter mensagens criptografadas para seu formato original.
+- **Tabela de Códigos**: Utiliza uma tabela específica para conversão de caracteres.
+
+## Tecnologias Utilizadas
+
+- **HTML**: Estrutura da página.
+- **CSS**: Estilização da página.
+- **JavaScript**: Lógica de criptografia e descriptografia.
+
+## Tabela de Códigos
+
+A tabela de códigos utilizada para criptografia e descriptografia é a seguinte:
+
+| Letra | Código |
+|-------|--------|
+| a     | X1     |
+| A     | X27    |
+| b     | Z2     |
+| B     | Z28    |
+| c     | Q3     |
+| C     | Q29    |
+| d     | W4     |
+| D     | W30    |
+| e     | T5     |
+| E     | T31    |
+| f     | V6     |
+| F     | V32    |
+| g     | R7     |
+| G     | R33    |
+| h     | P8     |
+| H     | P34    |
+| i     | S9     |
+| I     | S35    |
+| j     | Y10    |
+| J     | Y36    |
+| k     | U11    |
+| K     | U37    |
+| l     | M12    |
+| L     | M38    |
+| m     | N13    |
+| M     | N39    |
+| n     | O14    |
+| N     | O40    |
+| o     | L15    |
+| O     | L41    |
+| p     | K16    |
+| P     | K42    |
+| q     | J17    |
+| Q     | J43    |
+| r     | I18    |
+| R     | I44    |
+| s     | H19    |
+| S     | H45    |
+| t     | G20    |
+| T     | G46    |
+| u     | F21    |
+| U     | F47    |
+| v     | E22    |
+| V     | E48    |
+| w     | D23    |
+| W     | D49    |
+| x     | C24    |
+| X     | C50    |
+| y     | Y10    |
+| Y     | Y36    |
+| z     | A26    |
+| Z     | A52    |
+| *     | Espaço |
+
+## Instalação e Uso
+
+1. **Clone o repositório**:
+    ```bash
+    git clone https://github.com/devJunr/criptografador_de_mensagens
+    ```
+
+2. **Abra o arquivo `index.html` em seu navegador** para acessar a aplicação.
+
+3. **Digite a mensagem que deseja criptografar ou descriptografar** e clique nos botões apropriados.
+
+Você também pode visualizar a versão live do projeto, em: https://devjunr.github.io/criptografador_de_mensagens/
+
+## Agradecimentos
+
+Agradeço à Oracle Brasil e à Alura por fornecerem o programa ONE e por me ofertarem uma das vagas. Este repositório se trata de um desafio (Challenge) para que eu possa enviar para avaliação. Desde já, expresso minha sincera gratidão.
+
+![](https://i0.wp.com/pcformat.mx/www/wp-content/uploads/2022/03/Oracle-ONE.jpg?fit=1000%2C611&ssl=1)
+
+---
+<center>
+
+Feito por: [DevJunr](https://github.com/devjunr)
+
+</center>
--- app.js
+++ app.js
@@ -1,26 +1,63 @@
-let campoTexto = document.getElementById('entradaTexto').value;
+var textoCriptografado;
 
 function criptografarTexto(){
-        document.getElementById('saida_texto_imagem').style.display = 'none';
-        document.getElementById('saida_texto_frase').style.display = 'none';
-        document.getElementById('saida_texto_mensagem-aviso').style.display = 'none';
-        document.getElementById('saida_texto_mensagem').style.display = 'inline';
-        document.getElementById('copiarCriptografia').style.display = 'inline';
-        let textoCriptografar = document.getElementById('entradaTexto').value; // Captura o texto para cript
-        console.log(textoCriptografar);
-        document.getElementById('saida_texto_mensagem').style.color = '#495057';
-        let texto = document.getElementById('saida_texto_mensagem');
-        texto.innerHTML = textoCriptografar;
+    document.getElementById('saida_texto_imagem').style.display = 'none';
+    document.getElementById('saida_texto_frase').style.display = 'none';
+    document.getElementById('saida_texto_mensagem-aviso').style.display = 'none';
+    document.getElementById('saida_texto_mensagem').style.display = 'inline';
+    document.getElementById('copiarCriptografia').style.display = 'inline';
+    let textoCriptografar = document.getElementById('entradaTexto').value;
+    console.log(textoCriptografar);
+    let saida = document.getElementById('saida_texto_mensagem'); 
+    var textoCriptografado = textoCriptografar
+    .replace(/e/g, "enter")
+    .replace(/i/g,"imes")
+    .replace(/a/g,"ai")
+    .replace(/o/g,"ober")
+    .replace(/u/g,"ufat")  
+    saida.innerHTML = textoCriptografado;
+    let botaoPrint = document.getElementById('copiarCriptografia');
+    botaoPrint.innerHTML = 'Copiar';
 }
 
 function descriptografarTexto(){
-    alert("Botão para descriptografar texto");
+    document.getElementById('saida_texto_imagem').style.display = 'none';
+    document.getElementById('saida_texto_frase').style.display = 'none';
+    document.getElementById('saida_texto_mensagem-aviso').style.display = 'none';
+    document.getElementById('saida_texto_mensagem').style.display = 'inline';
+    document.getElementById('copiarCriptografia').style.display = 'inline';
+    let textoDescriptografar = document.getElementById('entradaTexto').value;
+    console.log(textoDescriptografar);
+    let saida = document.getElementById('saida_texto_mensagem');
+    var textoDesriptografado = textoDescriptografar
+    .replace(/enter/g, "e")
+    .replace(/imes/g,"i")
+    .replace(/ai/g,"a")
+    .replace(/ober/g,"o")
+    .replace(/ufat/g,"u")
+    saida.innerHTML = textoDesriptografado;
+    let botaoPrint = document.getElementById('copiarCriptografia');
+    botaoPrint.innerHTML = 'Copiar';
 }    
 
 function recarregarPagina(){
     location.reload();
 }
 
-/*
-Acrescentar criptografia da mensagem, criar a função do botão copiar, e acrescentar a estilização do textarea do campo de criptografia
-*/
+function avisoRegras(){
+    let textoDigitado = document.getElementById('entradaTexto').value;
+    let acentuado = /[A-ZÀ-ÿ-ỹ]/;
+    if(acentuado.test(textoDigitado)){
+        document.getElementById('aviso__regras__escrita').style.color = '#ff0000'; 
+    }else{
+        document.getElementById('aviso__regras__escrita').style.color = '#343A40';
+    }
+}
+
+function copiarTexto(){
+    let textoCopiar = document.getElementById('saida_texto_mensagem').value;
+    navigator.clipboard.writeText(textoCopiar);
+    let exibirCopiado = document.getElementById('copiarCriptografia');
+    let botaoPrint = document.getElementById('copiarCriptografia');
+    botaoPrint.innerHTML = 'Copiado com sucesso !';
+}
